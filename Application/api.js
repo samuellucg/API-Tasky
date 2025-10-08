d89dd5df8f2e2a597db01731b530a4f80e9f6882
Samuel Lucas <samuel.lucasmacedos@gmail.com>
2025-10-07 21:24:05 -0300

    Esqueleto para requisiçoes com telegram (apenas get em tarefas por enquanto)

--- Application/api.js
+++ Application/api.js
@@ -6,7 +6,12 @@ const fsNotPromises = require('fs');
 const fsPromisses = require('fs/promises');
 const crypto = require('crypto');
 const { FILE } = require('dns');
-
+const TelegramBot = require('node-telegram-bot-api');
+const bot = new TelegramBot('8191935582:AAGRVFfcJzXN0kt4FdjmiiCyt31BOvY5P9o');
+(async () => {
+    await bot.setWebHook("https://noneastern-lillian-tranquilly.ngrok-free.dev/api/telegram/webhook");
+    console.log("Webhook defined");
+})
 const app = express();
 const fs = fsPromisses;
 const fsSync = fsNotPromises;
@@ -22,7 +27,6 @@ const resJson = [{ICreate: "ApiTasky", Who: "SL MADE THIS"}, {NotifcationBy: "FC
 var hasInitialized = false;
 var hashToSave;
 var tasks;
-
 app.use(express.json());
 
 //#endregion
@@ -95,13 +99,14 @@ app.put("/tasks", async (req,res) => {
             toSave.TaskDesc = req.body.TaskDesc;
             toSave.HourTask = req.body.HourTask;
             toSave.NotifyTask = req.body.NotifyTask;
-            
+            toSave.TaskDone = req.body.TaskDone;
             tasks.forEach(async element => {
                 if(element.TaskId == Number(req.query.taskId)){
                     element.TaskName = toSave.TaskName;
                     element.TaskDesc = toSave.TaskDesc;
                     element.HourTask = toSave.HourTask;
                     element.NotifyTask = toSave.NotifyTask;
+                    element.TaskDone = toSave.TaskDone;
                     await fs.writeFile(FILENAME,JSON.stringify(tasks));
                     return res.status(200).send("OK");
                 } 
@@ -118,7 +123,68 @@ app.put("/tasks", async (req,res) => {
 
 //#endregion
 
+//#region Telegram Endpoints
+
+app.post("/api/telegram/webhook", async (req,res) => {
+    console.log(req.body.message.text);
+    if (req.body.message.text === '/tarefas') 
+    {
+        try {
+            const read = await fetch(`http://localhost:${PORT}/tasks`);
+            console.log("here");
+            const tasks = await read.json();                
+            console.log("here 2");
+
+            let message = "🗒️ *Suas tarefas:*\n\n";
+            console.log("here 3");
+
+            tasks.forEach(task => {
+                message += `📋 *Tarefa:* ${task.TaskName}\n`;
+                // message += `📅 *Data:* ${new Date(task.HourTask).toLocaleDateString('pt-BR')}\n`;
+                message += `📅 *Data:* ${task.HourTask}\n`;
+                if(task.TaskDone)
+                    message += `✅ *Concluída:* Sim\n\n`;
+                else
+                    message += `❌ *Concluída:* Não\n\n`
+            });
+
+            await bot.sendMessage(req.body.message.chat.id, message, { parse_mode: "Markdown" });
+
+            res.sendStatus(200);
+        } 
+        catch (error) {
+            console.error("Erro ao buscar tarefas:", error);
+            await bot.sendMessage(req.body.message.chat.id, "❌ Erro ao carregar as tarefas.");
+            res.sendStatus(500);
+        }
+    }
+     
+});
+
+//#endregion
+
 //#region Functions
+
+async function CreateTelegramData(telData){
+    if(telData != undefined){
+        const dataToWrite = [{"Id": telData.id, "Name" : `${telData.first_name} ${telData.last_name}`}]
+        if(fsSync.existsSync("TelegramData.json"))
+        {
+            const data = await fs.readFile("TelegramData.json",'utf8');
+            var dataa = await JSON.parse(data); 
+            // fazer verificação caso usuário já exista.
+            if(dataa != undefined){
+                dataa.push(...dataToWrite);
+                console.log("aqui");
+                fs.writeFile("TelegramData.json",JSON.stringify(dataa));
+            }
+        }
+        else{
+            fs.writeFile("TelegramData.json",JSON.stringify(dataToWrite));
+        }
+    }
+}
+
 
 async function startApi(){
     tasks = await ReadAndReturnJson();
@@ -153,7 +219,7 @@ async function ReadAndReturnJson(){
 async function DataIntoDatabase(){
     console.log(`${FILENAME} don't exist, creating...`);
     const firstObject = [{ "TaskId": 1, "TaskName": "Tarefa generica", "NotifyTask" : false, "TaskDesc": "DescGenerica", "HourTask": "2080-12-21T00:00:00-04:00",                
-        "IsEditingTask": false, "CanChange": true
+        "IsEditingTask": false, "CanChange": true, "TaskDone" : false
     }]
     fs.writeFile(FILENAME,JSON.stringify(firstObject));        
     console.log(`${FILENAME} created.`);    
